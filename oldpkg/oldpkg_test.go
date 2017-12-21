7a7def3195b2289a2aa5dd0f6ea389dda2cbf835
robert.hao <hypermind@outlook.com>
2017-12-21 12:30:25 +0800

    fix typo.

--- oldpkg/oldpkg_test.go
+++ oldpkg/oldpkg_test.go
@@ -10,9 +10,9 @@ func TestOldType(t *testing.T) {
 	var old OldType
 	switch tt := interface{}(old).(type) {
 	case newpkg.NewType:
-		t.Logf("The type of 'old' is equal to '%T'.", old)
+		t.Logf("The type of 'old' is equals to '%T'.", old)
 	default:
-		t.Errorf("The type of 'old' is not conform to the rules: %T", tt)
+		t.Errorf("The type of 'old' does not conform to the rules: %T", tt)
 
 	}
 }
